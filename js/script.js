df9ef0759866cb870173dec9b84498855ae0d9bc
Yasin Cakal <whoisyasin@gmail.com>
2025-04-04 17:06:04 -0700

    added hamburger menu functionality

--- js/script.js
+++ js/script.js
@@ -5,54 +5,77 @@ import sushi12 from '../assets/sushi-12.png'
 import sushi11 from '../assets/sushi-11.png'
 import sushi10 from '../assets/sushi-10.png'
 
-import AOS from "aos";
-import "aos/dist/aos.css";
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 // init AOS animation
 AOS.init({
-    duration: 1000,
-    offset: 100,
-});
+  duration: 1000,
+  offset: 100
+})
 
 const trendingSushis = [
-    'Make Sushi',
-    'Nigiri Sushi',
-    'Oshizushi',
-    'Temaki Sushi',
-    'Uramaki Sushi',
-    'Inari Sushi'
-];
+  'Make Sushi',
+  'Nigiri Sushi',
+  'Oshizushi',
+  'Temaki Sushi',
+  'Uramaki Sushi',
+  'Inari Sushi'
+]
 
 const trendingDrinks = [
-    "Oruncha",
-    "Ofukucha",
-    "Sakura Tea",
-    "Kombu-cha",
-    "Aojiru",
-    "Mugicha",
+  'Oruncha',
+  'Ofukucha',
+  'Sakura Tea',
+  'Kombu-cha',
+  'Aojiru',
+  'Mugicha'
 ]
 
 const cards = [
-    {
-        imgSrc: sushi12,
-        alt: "sushi-12",
-        title: "Chezu Sushi",
-        rating: "4.8",
-        price: "$21.00"
-    },
-    {
-        imgSrc: sushi11,
-        alt: "sushi-11",
-        title: "Originale Sushi",
-        rating: "4.8",
-        price: "$21.00",
-        active: true
-    },
-    {
-        imgSrc: sushi10,
-        alt: "sushi-10",
-        title: "Ramen Legendo",
-        rating: "4.8",
-        price: "$21.00"
-    }
-];
+  {
+    imgSrc: sushi12,
+    alt: 'sushi-12',
+    title: 'Chezu Sushi',
+    rating: '4.8',
+    price: '$21.00'
+  },
+  {
+    imgSrc: sushi11,
+    alt: 'sushi-11',
+    title: 'Originale Sushi',
+    rating: '4.8',
+    price: '$21.00',
+    active: true
+  },
+  {
+    imgSrc: sushi10,
+    alt: 'sushi-10',
+    title: 'Ramen Legendo',
+    rating: '4.8',
+    price: '$21.00'
+  }
+]
+
+// Select the hamburger icon and mobile menu dropdown
+const mobileMenuIcon = document.querySelector('.header__menu-mobile li img')
+const mobileMenu = document.getElementById('mobileMenu')
+
+// Toggle mobile menu on icon click
+mobileMenuIcon.addEventListener('click', () => {
+  if (
+    mobileMenu.style.display === 'flex' ||
+    mobileMenu.style.display === 'block'
+  ) {
+    mobileMenu.style.display = 'none'
+  } else {
+    mobileMenu.style.display = 'block'
+  }
+})
+
+// Close the menu when a link is clicked
+mobileMenu.addEventListener('click', e => {
+  if (e.target.tagName === 'A') {
+    mobileMenu.style.display = 'none'
+  }
+})
